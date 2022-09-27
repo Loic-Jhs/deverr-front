299f29804ec3841b1eb4a370930a7cc69cc32a45
Loic Jouhans <loic.jouhans@yahoo.fr>
2022-09-27 16:06:06 +0200

    cleaning code after merging
    Signed-off-by: Loic Jouhans <loic.jouhans@yahoo.fr>

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { Routes, Route } from 'react-router-dom';
-import { useState } from 'react'
-import { Routes, Route } from 'react-router-dom';
 
 import './App.css'
 import Header from './components/Header/Header'
