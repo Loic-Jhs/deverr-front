71a4d4e94f19381b854c437abb670015bb0c3575
Shiroi Kiba <miletgiovanni@gmail.com>
2022-10-25 20:41:16 +0200

    Add new img Comment one route (need to define if we need 2 routes or one with a condition)

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,12 @@ import './App.css'
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import Homepage from './components/Homepage/Homepage';
-import Register from './components/Register/Register';
 import DevDetail from './components/DevDetail/DevDetail';
 import DevList from './components/DevList/DevList';
 import FormDev from './components/FormDev/FormDev';
 import FormClient from './components/FormClient/FormClient';
+import Register from './components/Register/Register';
+import DevProfile from './components/DevProfile/DevProfile';
 
 function App() {
   return (
@@ -17,8 +18,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/register" element={<Register />} />
-        <Route path="dev-profile/:userId" element={<DevDetail />} />
-        <Route path="developpeurs" element={<DevList />} />
+        <Route path="/dev-profile/:devID" element={<DevProfile />} />
+        {/* <Route path="dev-pro/:devID" element={<DevDetail />} />*/}        
+        <Route path="developers" element={<DevList />} />
         <Route path="/registerdev" element={<FormDev />} />
         <Route path="/registerclient" element={<FormClient />} />
       </Routes>
