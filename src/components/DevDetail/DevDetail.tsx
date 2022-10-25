d6e567ba1c0e63fc731819f43f58484db861a202
Shiroi Kiba <miletgiovanni@gmail.com>
2022-10-25 20:44:23 +0200

    use real endpoint for the detail of a dev

--- src/components/DevDetail/DevDetail.tsx
+++ src/components/DevDetail/DevDetail.tsx
@@ -1,21 +1,48 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { DevInfos } from '../../types';
 import DevPrestation from '../DevPrestation/DevPrestation';
 import ProfileInfo from '../ProfileInfo/ProfileInfo';
 import StackComment from '../StackComment/StackComment';
 import './style.scss';
 
 function DevDetail() {
-    const { userId } = useParams();
+    const { devID } = useParams();
+    const [dev, setDev] = useState<DevInfos>()
+    const [isLoaded, setIsLoaded] = useState<Boolean>(false);
 
-    return (
-        <div className='dev__container'>
-            <div className="profile-prestation__container">
-            <ProfileInfo devId={Number(userId)}/>
-            <DevPrestation devId={Number(userId)} />
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await fetch(`http://api-dev.deverr.fr/developer-details/${devID}`, {
+              method: "GET",
+              headers: {
+                "access-control-allow-origin" : "*",
+                "Content-type": "application/json"
+              },
+            mode: 'cors'});
+            const data = await response.json();
+            setDev(data);
+            setIsLoaded(true);
+            console.log(dev)
+          } catch (e) {
+            console.log(e)
+          }
+        }
+        fetchData()
+    }, [isLoaded])
+
+    
+        return (
+            
+            <div className='dev__container'>
+                <div className="profile-prestation__container">
+                <ProfileInfo devID={Number(devID)}/>
+                <DevPrestation devID={Number(devID)} />
+                </div>
+                <StackComment devID={Number(devID)} />
             </div>
-            <StackComment devId={Number(userId)} />
-        </div>
-    );
+        );
 }
 
 export default DevDetail;
