a4ce03e238ca0afb133ce446468a27a6577805c9
Loic Jouhans <loic.jouhans@yahoo.fr>
2022-11-02 03:11:02 +0100

    clean code in App.tsx
    Signed-off-by: Loic Jouhans <loic.jouhans@yahoo.fr>

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,32 @@
-import { Routes, Route } from 'react-router-dom';
-import { useContext, useEffect, useState } from 'react';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
+import FormClient from './components/FormClient/FormClient';
+import DevProfile from './components/DevProfile/DevProfile';
+import DevOrder from './components/DevOrder/DevOrder';
 import Homepage from './components/Homepage/Homepage';
+import Register from './components/Register/Register';
 import DevList from './components/DevList/DevList';
 import FormDev from './components/FormDev/FormDev';
-import FormClient from './components/FormClient/FormClient';
-import Register from './components/Register/Register';
-import DevProfile from './components/DevProfile/DevProfile';
+import { Routes, Route } from 'react-router-dom';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 import Login from "./components/Login/Login";
-import User from './types/User';
 import './App.css'
-import { UserAsContext } from './types';
-import { authContext } from './contexts/authContext';
-import DevOrder from './components/DevOrder/DevOrder';
 
 function App() {
-  const { auth} = useContext(authContext)
-  const [isLogged, setIsLogged] = useState(false)
 
   return (
     <div className="App">
-        <Header />
-          <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/dev-profile/:devID" element={<DevProfile />} />
-            <Route path="/dev-order/:devID" element={<DevOrder />} />
-            <Route path="developers" element={<DevList />} />
-            <Route path="/registerdev" element={<FormDev />} />
-            <Route path="/registerclient" element={<FormClient />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        <Footer />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dev-profile/:devID" element={<DevProfile />} />
+        <Route path="/dev-order/:devID" element={<DevOrder />} />
+        <Route path="developers" element={<DevList />} />
+        <Route path="/registerdev" element={<FormDev />} />
+        <Route path="/registerclient" element={<FormClient />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
     </div>
   )
 }
