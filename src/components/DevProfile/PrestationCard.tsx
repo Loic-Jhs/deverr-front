6d82c2605aff0e558176bc0412c60ff94f43dfc3
Loic Jouhans <loic.jouhans@yahoo.fr>
2022-12-30 23:49:37 +0100

    move files to the right component

---
+++ src/components/DevProfile/PrestationCard.tsx
@@ -0,0 +1,148 @@
+import React, {
+  Dispatch,
+  SetStateAction,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import { authContext } from "../../contexts/authContext";
+import { Input, TextareaAutosize } from "@mui/material";
+import TaskAltIcon from "@mui/icons-material/TaskAlt";
+import CancelIcon from "@mui/icons-material/Cancel";
+import EditIcon from "@mui/icons-material/Edit";
+import type { RealPrestation } from "../../types";
+import { useParams } from "react-router-dom";
+
+type PROPS = {
+  prestation: RealPrestation;
+  services: Boolean;
+  setServices: Dispatch<SetStateAction<Boolean>>;
+  devProfileId?: Number;
+};
+
+function PrestationCard(props: PROPS) {
+  //HOOKS
+  const { devID } = useParams();
+  const { auth } = useContext(authContext);
+
+  //STATES
+  const [price, setPrice] = useState<number>(props.prestation.price);
+  const [editService, setEditService] = useState<Boolean>(false);
+  const [description, setDescription] = useState<string | undefined>(props.prestation.description);
+
+  const updateService = (id: number, description?: string, price?: number) => {
+    fetch(`http://localhost/profile/prestations/edit/${id}`, {
+      method: "PATCH",
+      headers: {
+        Authorization: "Bearer " + auth.access_token,
+        "access-control-allow-origin": "*",
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        prestation_type_id: id,
+        description: description,
+        price: price,
+      }),
+      mode: "cors",
+    })
+      .then((_response) => {
+        props.setServices(!props.services);
+        setEditService(false);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const deleteService = (id: number) => {
+    fetch(`http://localhost/profile/prestations/delete/${id}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: "Bearer " + auth.access_token,
+        "access-control-allow-origin": "*",
+        "Content-type": "application/json",
+      },
+      mode: "cors",
+    })
+      .then((_response) => props.setServices(!props.services))
+      .catch((error) => console.log(error));
+  };
+
+  console.log(auth.user_info);
+  
+
+  return (
+    <div className="dev__prestation-item">
+      <div className="edit">
+        <h4>{props.prestation.name}</h4>
+        {
+          /*
+          Si le user connecté correspond à un développeur 
+          on affiche les boutons
+          */
+          auth.user_info.user_id === props.devProfileId &&
+          <div className="svg">
+            {!editService && (
+              <>
+                <EditIcon
+                  color="primary"
+                  onClick={() => {
+                    setEditService(true);
+                  }}
+                />
+                <DeleteForeverIcon
+                  color="error"
+                  onClick={() => deleteService(props.prestation.id)}
+                />
+              </>
+            )}
+            {editService && (
+              <>
+                <TaskAltIcon
+                  color="primary"
+                  onClick={() =>
+                    updateService(props.prestation.id, description, price)
+                  }
+                />
+                <CancelIcon color="error" onClick={() => setEditService(false)} />
+              </>
+            )}
+          </div>
+        }
+      </div>
+      {!editService && (
+        <>
+          <p className="p__detail">Details :</p>
+          <p className="prestation__description">
+            {props.prestation.description}
+          </p>
+          <p className="dev__prestation-price">
+            <span>Tarif :</span> {props.prestation.price} €
+          </p>
+        </>
+      )}
+      {editService && (
+        <div className="editable__input">
+          <TextareaAutosize
+            maxRows={4}
+            aria-label="maximum height"
+            placeholder="Maximum 4 rows"
+            defaultValue={description}
+            style={{ width: 200 }}
+            onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) =>
+              setDescription(event.target.value)
+            }
+          />
+          <Input
+            type="number"
+            defaultValue={price}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              setPrice(Number(event.target.value))
+            }
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default PrestationCard;
---
+++ src/components/DevProfile/ServicesModal.tsx
@@ -0,0 +1,138 @@
+import React, {
+  Dispatch,
+  SetStateAction,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { authContext } from "../../contexts/authContext";
+import TextField from "@mui/material/TextField";
+import ModalWindow from "../Modal/Modal";
+import Button from "../Button/Button";
+
+type  ModalType = {
+  open: boolean;
+  onClose: () => void;
+  services: Dispatch<SetStateAction<Boolean>>;
+}
+
+type T = {
+  id: number;
+  name: string;
+};
+
+function ServicesModal(props: ModalType) {
+  //HOOKS
+  const { auth } = useContext(authContext);
+
+  //STATES
+  const [selected, setSelected] = useState<number | undefined>(undefined);
+  // TODO change for the good type
+  const [serviceSelected, setServiceSelected] = useState<any>();
+  const [description, setDescription] = useState<string | undefined>();
+  const [price, setPrice] = useState<string | undefined>();
+  const [data, setData] = useState<T[]>();
+
+  useEffect(() => {
+    fetch(`http://localhost/all-prestations/`, {
+      method: "GET",
+      headers: {
+        "access-control-allow-origin": "*",
+        "Content-type": "application/json",
+      },
+      mode: "cors",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
+
+  const handleClick = (item: T) => {
+    setSelected(item.id);
+    setServiceSelected(item.id);
+  };
+
+  const validateAddingService = () => {
+    fetch(`http://localhost/profile/prestations/store`, {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + auth.access_token,
+        "access-control-allow-origin": "*",
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        prestation_type_id: serviceSelected,
+        description: description,
+        price: Number(price),
+      }),
+      mode: "cors",
+    })
+      .then((_response) => {
+        props.services(true);
+        props.onClose();
+      })
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <ModalWindow
+      open={props.open}
+      onClose={props.onClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <div>
+        <h2>Choisissez une prestation</h2>
+        <ul>
+          {data &&
+            data.map((item: T) => {
+              return (
+                <div key={item.id}>
+                  <li
+                    onClick={() => handleClick(item)}
+                    value={item.id}
+                    className={selected === item.id ? "selected" : ""}
+                  >
+                    {item.name}
+                  </li>
+                  {selected === item.id && (
+                    <div>
+                      <TextField
+                        id="standard-basic"
+                        margin="dense"
+                        label="Description"
+                        variant="standard"
+                        onChange={(
+                          event: React.ChangeEvent<HTMLInputElement>
+                        ) => setDescription(event.target.value)}
+                      />
+                      <TextField
+                        id="standard-basic"
+                        margin="dense"
+                        label="Prix en €"
+                        variant="standard"
+                        type="number"
+                        onChange={(
+                          event: React.ChangeEvent<HTMLInputElement>
+                        ) => setPrice(event.target.value)}
+                      />
+                    </div>
+                  )}
+                </div>
+              );
+            })}
+        </ul>
+        <Button
+          onClick={() => {
+            validateAddingService();
+          }}
+        >
+          Valider
+        </Button>
+      </div>
+    </ModalWindow>
+  );
+}
+
+export default ServicesModal;
