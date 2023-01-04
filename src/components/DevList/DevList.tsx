b7a55c3f0dbbcc95554b34197cf735056bd440d0
Loic Jouhans <loic.jouhans@yahoo.fr>
2023-01-04 21:59:50 +0100

    rework of input search for find a dev in DevList

--- src/components/DevList/DevList.tsx
+++ src/components/DevList/DevList.tsx
@@ -8,6 +8,8 @@ function DevList() {
   const [query, setQuery] = useState("");
   const [devList, setDevList] = useState<Dev[]>();
   const [isLoaded, setIsLoaded] = useState<Boolean>(false);
+  const [filteredDev, setFilteredDev] = useState<Dev[]>();
+
   useEffect(() => {
     const fetchData = async () => {
       await fetch("http://localhost/all-developers", {
@@ -21,6 +23,7 @@ function DevList() {
         .then((response) => response.json())
         .then((data) => {
           setDevList(data);
+          setFilteredDev(data);
           setIsLoaded(true);
         })
         .catch((e) => console.log(e));
@@ -28,125 +31,130 @@ function DevList() {
     fetchData();
   }, [isLoaded]);
 
+  const filterDevFunc = (devArray: Dev[] | undefined, inputLetters: string) => {
+    // Si input vide
+    if (inputLetters === "") {
+      // on renvoie le tableau initial (càd tous les dev)
+      return devArray;
+    } else {
+      // On filtre le tableau de dev
+      return devArray?.filter((user) =>
+        // Vérifie la condition, si true renvoie le dev, sinon rien
+        user.stacks.some((stack) =>
+          // Si une lettre match avec une stack.name, renvoi true ⬆
+          stack.name.toLowerCase().includes(inputLetters.toLowerCase())
+        )
+      );
+    }
+  };
+
   return (
     <>
       <div className="search">
-        <label htmlFor="searchTechno">Rechercher une technologie</label>
+        <label htmlFor="searchTechno">
+          Rechercher un(e) Dev(e) par technologie
+        </label>
         <input
           name="searchTechno"
           placeholder="PHP, JavaScript..."
-          onChange={(event) => setQuery(event.target.value)}
+          onChange={(event) => {
+            setQuery(event.target.value);
+            const filterDevSearch = filterDevFunc(devList, event.target.value);
+            setFilteredDev(filterDevSearch);
+          }}
         />
       </div>
       <div className="dev-list__container">
-        {devList &&
-          devList
-            .filter((dev) => {
-              if (query === "") {
-                //if query is empty
-                return dev;
-              } else if (dev.stacks !== null && dev.stacks.length > 0) {
-                let devStacks: string[] = [];
-                dev.stacks.map((stack) => {
-                  devStacks.push(stack.name.toLowerCase());
-                });
-                if (devStacks.includes(query.toLowerCase())) {
-                  return dev;
-                }
-              }
-            })
-            .map((dev) => {
-              const {
-                id,
-                firstname,
-                lastname,
-                avatar,
-                average_rating,
-                description,
-                prestations,
-                stacks,
-                reviews_number,
-              } = dev;
+        {filteredDev &&
+          filteredDev.map((dev) => {
+            const {
+              id,
+              firstname,
+              lastname,
+              avatar,
+              average_rating,
+              description,
+              prestations,
+              stacks,
+              reviews_number,
+            } = dev;
 
-              return (
-                <Link
-                  key={id}
-                  to={`/dev-profile/${id}`}
-                  className="dev-list__link"
-                >
-                  <div className="dev__info">
-                    <div className="dev__info-picture-fav">
-                      <img src={`${avatar}`} alt={`${firstname} avatar`} />
-                    </div>
-                    <div className="dev__name-rate">
-                      <p className="dev__name">
-                        {firstname} {lastname}
-                      </p>
-                      {average_rating != null ? (
-                        <div className="dev__rate">
-                          <Rating
-                            name="half-rating-read"
-                            size="large"
-                            value={average_rating}
-                            precision={0.5}
-                            readOnly
-                          />
-                          <p>{average_rating}</p>
-                          <p>({reviews_number})</p>
-                        </div>
-                      ) : (
-                        <div className="dev__rate">
-                          <Rating
-                            name="half-rating-read"
-                            size="large"
-                            value={0}
-                            precision={0.5}
-                            readOnly
-                          />
-                          <p>Aucune note</p>
-                        </div>
-                      )}
-                    </div>
-                    <p className="dev__description">
-                      {description.slice(0, 100)}...
+            return (
+              <Link
+                key={id}
+                to={`/dev-profile/${id}`}
+                className="dev-list__link"
+              >
+                <div className="dev__info">
+                  <div className="dev__info-picture-fav">
+                    <img src={`${avatar}`} alt={`${firstname} avatar`} />
+                  </div>
+                  <div className="dev__name-rate">
+                    <p className="dev__name">
+                      {firstname} {lastname}
                     </p>
+                    {average_rating != null ? (
+                      <div className="dev__rate">
+                        <Rating
+                          name="half-rating-read"
+                          size="large"
+                          value={average_rating}
+                          precision={0.5}
+                          readOnly
+                        />
+                        <p>{average_rating}</p>
+                        <p>({reviews_number})</p>
+                      </div>
+                    ) : (
+                      <div className="dev__rate">
+                        <Rating
+                          name="half-rating-read"
+                          size="large"
+                          value={0}
+                          precision={0.5}
+                          readOnly
+                        />
+                        <p>Aucune note</p>
+                      </div>
+                    )}
                   </div>
-                  <div className="dev__stacks-prestations">
-                    <h2>Mes compétences :</h2>
-                    <div className="dev__stacks">
-                      {stacks &&
-                        stacks.map((stack) => {
-                          return (
-                            <div
-                              key={stack.id}
-                              className="dev__item-stack-logo"
-                            >
-                              <img
-                                src={`${stack.logo}`}
-                                alt={`${stack.name} logo`}
-                              />
-                            </div>
-                          );
-                        })}
-                    </div>
-                    <h2>Mes prestations :</h2>
-                    <div className="dev__prestations">
-                      {prestations &&
-                        prestations.map((prestation) => {
-                          return (
-                            <div
-                              key={prestation.id}
-                              className="prestation__container"
-                            >
-                              <p>{prestation.name}</p>
-                            </div>
-                          );
-                        })}
-                    </div>
+                  <p className="dev__description">
+                    {description.slice(0, 100)}...
+                  </p>
+                </div>
+                <div className="dev__stacks-prestations">
+                  <h2>Mes compétences :</h2>
+                  <div className="dev__stacks">
+                    {stacks &&
+                      stacks.map((stack) => {
+                        return (
+                          <div key={stack.id} className="dev__item-stack-logo">
+                            <img
+                              src={`${stack.logo}`}
+                              alt={`${stack.name} logo`}
+                            />
+                          </div>
+                        );
+                      })}
                   </div>
-                </Link>
-              );
-            })}
+                  <h2>Mes prestations :</h2>
+                  <div className="dev__prestations">
+                    {prestations &&
+                      prestations.map((prestation) => {
+                        return (
+                          <div
+                            key={prestation.id}
+                            className="prestation__container"
+                          >
+                            <p>{prestation.name}</p>
+                          </div>
+                        );
+                      })}
+                  </div>
+                </div>
+              </Link>
+            );
+          })}
       </div>
     </>
   );
